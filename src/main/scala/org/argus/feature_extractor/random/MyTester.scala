01d0a1ab1c121178aff2eb8b966ae6b66af53938
octacode <allblue.shasha@gmail.com>
2018-07-13 11:22:53 -0400

    Completed module 1

--- src/main/scala/org/argus/feature_extractor/random/MyTester.scala
+++ src/main/scala/org/argus/feature_extractor/random/MyTester.scala
@@ -1,14 +1,17 @@
 package org.argus.feature_extractor.random
 
-import java.io.PrintWriter
-
 import org.argus.amandroid.alir.componentSummary.ApkYard
+import org.argus.amandroid.core.ApkGlobal
 import org.argus.amandroid.core.decompile.{DecompileLayout, DecompileStrategy, DecompilerSettings}
-import org.argus.jawa.alir.JawaAlirInfoProvider
+import org.argus.feature_extractor.{AllPermissions, AllReceiver}
 import org.argus.jawa.core.DefaultReporter
 import org.argus.jawa.core.util._
 
 object MyTester {
+  var apk: ApkGlobal = _
+  var permMap: MLinkedMap[String, Integer] = AllPermissions.hashMap
+  var recvMap: MLinkedMap[String, Integer] = AllReceiver.hashMap
+
   def main(args: Array[String]): Unit = {
     if (args.length != 2) {
       println("usage: apk_path output_path")
@@ -21,26 +24,32 @@ object MyTester {
     val layout = DecompileLayout(outputUri)
     val strategy = DecompileStrategy(layout)
     val settings = DecompilerSettings(debugMode = true, forceDelete = true, strategy, reporter)
-    val apk = yard.loadApk(fileUri, settings, collectInfo = true, resolveCallBack = false)
+    apk = yard.loadApk(fileUri, settings, collectInfo = true, resolveCallBack = false)
 
     val permissions = apk.model.getUsesPermissions
     val certificates = apk.model.getCertificates
-
-    val allMethods = apk.getApplicationClasses.map(c => c.getDeclaredMethods).reduce(_ ++ _)
-        allMethods.foreach { m =>
-          println(m.getSignature)
-          println(m.retrieveCode)
-          val cfg = JawaAlirInfoProvider.getCfg(m)
-          cfg.toDot(new PrintWriter(System.out))
-        }
-
-//    printMe("Permissions")(permissions.toList)
+    val services = apk.model.getServices
+    val receivers = apk.model.getReceivers
+    modAll(permissions)(permMap)
   }
 
-  def printMe(message: String)(item: List[String]): Unit = {
-    println("\t\t"+message)
+  def modAll(item: ISet[String])(hashMap: MLinkedMap[String, Integer]): Unit = {
     item.foreach {
-      hello => println(hello)
+      hello => {
+        if (hashMap.contains(hello)) {
+          hashMap.put(hello, 1)
+        }
+      }
+    }
+    printMap(hashMap)
+  }
+
+  def printMap(hashMap: MLinkedMap[String, Integer]): Unit = {
+    var set = hashMap.keySet
+    set.foreach {
+      hello => {
+        println(hello + "     " + hashMap(hello))
+      }
     }
   }
 }
